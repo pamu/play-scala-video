f3e5311a8551e92cef268cbe9ef994bca2fc6021
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-08-15 22:47:32 +0530

    video streaming

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,62 @@
+package controllers
+
+import java.io.{File, FileInputStream}
+import javax.inject._
+
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import play.api.http.HttpEntity
+import play.api.libs.MimeTypes
+import play.api.libs.iteratee.{Enumeratee, Enumerator}
+import play.api.libs.streams.Streams
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+@Singleton
+class HomeController @Inject() extends Controller {
+
+
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def media = Action { req =>
+    val file = new File("/Users/pnagarjuna/Downloads/passenger.mp4")
+    val rangeHeaderOpt = req.headers.get(RANGE)
+    rangeHeaderOpt.map { range =>
+      val strs = range.substring("bytes=".length).split("-")
+      if (strs.length == 1) {
+        val start = strs.head.toLong
+        val length = file.length() - 1L
+        partialContentHelper(file, start, length)
+      } else {
+        val start = strs.head.toLong
+        val length = strs.tail.head.toLong
+        partialContentHelper(file, start, length)
+      }
+    }.getOrElse {
+      Ok.sendFile(file)
+    }
+  }
+
+  def partialContentHelper(file: File, start: Long, length: Long) = {
+    val fis = new FileInputStream(file)
+    fis.skip(start)
+    val byteStringEnumerator = Enumerator.fromStream(fis).&>(Enumeratee.map(ByteString.fromArray(_)))
+    val mediaSource = Source.fromPublisher(Streams.enumeratorToPublisher(byteStringEnumerator))
+    PartialContent.sendEntity(HttpEntity.Streamed(mediaSource, None, None)).withHeaders(
+      CONTENT_TYPE -> MimeTypes.forExtension("mp4").get,
+      CONTENT_LENGTH -> ((length - start) + 1).toString,
+      CONTENT_RANGE -> s"bytes $start-$length/${file.length()}",
+      ACCEPT_RANGES -> "bytes",
+      CONNECTION -> "keep-alive"
+    )
+  }
+
+  def fileUpload = Action.async(parse.multipartFormData) { req =>
+    Future(Ok(""))
+  }
+
+}
