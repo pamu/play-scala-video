95697dbaa9f08ab5e3c34c95b33d937436aa9ae4
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-08-23 18:18:13 +0530

    file upload and chuck size function parameter

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -37,7 +37,7 @@ class HomeController @Inject() extends Controller {
       if (strs.length == 1) {
         val start = strs.head.toLong
         val length = file.length() - 1L
-        partialContentHelper(file, start, length)
+        partialContentHelper(file, start, length, Some(1024 * 16))
       } else {
         val start = strs.head.toLong
         val length = strs.tail.head.toLong
@@ -49,33 +49,30 @@ class HomeController @Inject() extends Controller {
   }
 
 
-  def partialContentHelper(file: File, start: Long, length: Long) = {
+  def partialContentHelper(file: File, start: Long, length: Long, optChunkSize: Option[Int] = None) = {
 
     val fis = new FileInputStream(file)
     fis.skip(start)
 
-    var count = 0
+    val byteStringEnumerator = optChunkSize.map { chunkSize =>
+      Enumerator.fromStream(fis, chunkSize)
+    }.getOrElse {
+      Enumerator.fromStream(fis)
+    }.map(ByteString.fromArray).onDoneEnumerating {
+      Try {
+        Logger.warn("file closed")
+        fis.close()
+      }
+    }
 
-    val byteStringEnumerator = Enumerator.fromStream(fis).map { bytes =>
-      count += 1
-      Logger.warn(s"count: $count. chunk size: ${bytes.length}")
-      ByteString.fromArray(bytes)
-    }.onDoneEnumerating(Try {
-      Logger.warn("file closed")
-      fis.close()
-    })
-
-    val countingEnumerator = byteStringEnumerator
-
-    val mediaSource = Source.fromPublisher(Streams.enumeratorToPublisher(countingEnumerator))
+    val mediaSource = Source.fromPublisher(Streams.enumeratorToPublisher(byteStringEnumerator))
 
     val partialContent = PartialContent.sendEntity(HttpEntity.Streamed(mediaSource, None, None)).withHeaders(
       CONTENT_TYPE -> MimeTypes.forExtension("mp4").get,
       CONTENT_LENGTH -> ((length - start) + 1).toString,
       CONTENT_RANGE -> s"bytes $start-$length/${file.length()}",
       ACCEPT_RANGES -> "bytes",
-      CONNECTION -> "keep-alive"
-    )
+      CONNECTION -> "keep-alive")
 
     Logger.warn(partialContent.header.headers.mkString("\n"))
 
@@ -83,7 +80,15 @@ class HomeController @Inject() extends Controller {
   }
 
   def fileUpload = Action.async(parse.multipartFormData) { req =>
-    Future(Ok(""))
+    Future {
+      scala.concurrent.blocking {
+        //TODO copy files to media folder
+      }
+    }.map { result =>
+      Ok("success")
+    }.recover { case throwable =>
+      Ok("failure")
+    }
   }
 
 }
